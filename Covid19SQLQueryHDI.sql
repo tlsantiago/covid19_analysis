5422c6e56755ee5168ed7368e988b8b5ec636cc6
Santiago <ssantiagotl@gmail.com>
2021-08-02 21:04:40 -0300

    Queries to evaluate each HDI tier 5 worst, 5 best performance countries

--- Covid19SQLQueryHDI.sql
+++ Covid19SQLQueryHDI.sql
@@ -11,3 +11,62 @@ DELETE FROM covid19.dbo.worldDataHDI
 WHERE location is NULL
 
 -- Evaluating different HDI performances
+-- Low HDI
+
+SELECT * FROM (
+SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'Low Human Development'
+ORDER BY DeathLikelyhood asc ) as A
+
+UNION
+
+SELECT * FROM (SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'Low Human Development'
+ORDER BY DeathLikelyhood desc) as B
+
+-- Medium HDI
+
+SELECT * FROM (
+SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'Medium Human Development'
+ORDER BY DeathLikelyhood asc ) as A
+
+UNION
+
+SELECT * FROM (SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'Medium Human Development'
+ORDER BY DeathLikelyhood desc) as B
+
+-- High HDI
+
+SELECT * FROM (
+SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'High Human Development'
+ORDER BY DeathLikelyhood asc ) as A
+
+UNION
+
+SELECT * FROM (SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'High Human Development'
+ORDER BY DeathLikelyhood desc) as B
+
+-- Very High HDI
+
+SELECT * FROM (
+SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'Very High Human Development'
+ORDER BY DeathLikelyhood asc ) as A
+
+UNION
+
+SELECT * FROM (SELECT Top 5 *
+FROM covid19.dbo.worldDataHDI
+WHERE HDI_tier = 'Very High Human Development'
+ORDER BY DeathLikelyhood desc) as B
